7eb2e3fa1da942436fc1105ca86f375a325be3e2
bhavy-sharma <bhavymay18@gmail.com>
2025-06-03 10:16:56 +0530

    Crouser responsive

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,23 +1,24 @@
 let currentSlide = 0;
-    const slides = document.querySelectorAll('.carousel-slide');
+const slides = document.querySelectorAll('.carousel-slide');
 
-    function showSlide(index) {
-      slides.forEach((slide, i) => {
-        slide.classList.remove('active');
-        if (i === index) slide.classList.add('active');
-      });
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove('active');
+    if (i === index) {
+      slide.classList.add('active');
     }
+  });
+}
 
-    function nextSlide() {
-      currentSlide = (currentSlide + 1) % slides.length;
-      showSlide(currentSlide);
-    }
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
 
-    function prevSlide() {
-      currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-      showSlide(currentSlide);
-    }
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+}
 
-    setInterval(() => {
-      nextSlide();
-    }, 6000);
+// Auto Slide (optional)
+setInterval(nextSlide, 5000);
