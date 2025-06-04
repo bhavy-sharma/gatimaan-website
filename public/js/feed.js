b47c2c9a3fe219a6d42cc7ecbab800f31b8c0fb3
Sjain0018 <sj5373557@gmail.com>
2025-06-05 02:04:25 +0530

    Fix feed

--- public/js/feed.js
+++ public/js/feed.js
@@ -3,12 +3,10 @@ function incrementNumbers() {
     const projects = document.getElementById('projects');
     const clients = document.getElementById('clients');
     const satisfaction = document.getElementById('satisfaction');
-    const team = document.getElementById('team');
 
     let projectCount = 0;
     let clientCount = 0;
     let satisfactionPercent = 0;
-    let teamCount = 0;
 
     const interval = setInterval(() => {
         if (projectCount <= 100) {
@@ -26,13 +24,8 @@ function incrementNumbers() {
             satisfactionPercent++;
         }
 
-        if (teamCount <= 30) {
-            team.textContent = `${teamCount}`;
-            teamCount++;
-        }
-
         // Stop incrementing when all values are reached
-        if (projectCount > 100 && clientCount > 200 && satisfactionPercent > 98 && teamCount > 30) {
+        if (projectCount > 100 && clientCount > 200 && satisfactionPercent > 98) {
             clearInterval(interval);
         }
     }, 100); // Adjust speed by changing interval time
