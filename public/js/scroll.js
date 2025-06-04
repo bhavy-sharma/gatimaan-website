f7913c2f6c2a5925ca410818933c2520c772cdea
Sjain0018 <sj5373557@gmail.com>
2025-06-05 03:07:14 +0530

    Add scrolling animation

---
+++ public/js/scroll.js
@@ -0,0 +1,24 @@
+  const boxes = document.querySelectorAll('.des');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry, index) => {
+        if (entry.isIntersecting) {
+          setTimeout(() => {
+            entry.target.classList.add('visible');
+          }, index * 250); 
+          observer.unobserve(entry.target); 
+        }
+      });
+    }, {
+      threshold: 0.1
+    });
+
+    boxes.forEach(box => observer.observe(box));
+
+    // On page reload
+
+  window.addEventListener('load', () => {
+    setTimeout(() => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }, 100);
+  });
